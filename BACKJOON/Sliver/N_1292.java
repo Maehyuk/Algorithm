0a006a79c933f084763acd845f9a0d075b686185
백재혁 <bjh010321@naver.com>
2021-08-10 14:53:54 +0900

    2021-08-10

---
+++ BACKJOON/Sliver/N_1292.java
@@ -0,0 +1,43 @@
+package Sliver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N_1292 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int arr[] = new int[2000];
+
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+
+		int index = 0;
+		int sum = 0;
+
+		for (int i = 1; i <= 1000; i++) {
+			for (int j = 0; j < i; j++) {
+
+				if (index == 1001) {
+					break;
+				}
+
+				index++;
+				arr[index] = i;
+
+			}
+		}
+
+		for (int i = A; i <= B; i++) {
+			sum += arr[i];
+		}
+
+		System.out.println(sum);
+
+	}
+
+}
