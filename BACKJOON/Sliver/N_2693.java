5e4af43b428b1ff3bbfc5d2de0fbfd3af6dfa4d9
백재혁 <bjh010321@naver.com>
2021-08-17 16:25:06 +0900

    2021-08-17

---
+++ BACKJOON/Sliver/N_2693.java
@@ -0,0 +1,28 @@
+package Sliver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class N_2693 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st ;
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<T; i++) {
+            st = new StringTokenizer(br.readLine()," ");
+            int arr [] = new int[10];
+
+            for(int j=0; j<10; j++) {
+                arr[j] = Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(arr);
+
+            System.out.println(arr[7]);
+        }
+    }
+}
